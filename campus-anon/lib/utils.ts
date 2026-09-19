83f1b98fb1d6673b473bd9ea3c9f338182a7367c
Shreyasht18 <ctolerate@gmail.com>
2026-09-19 11:03:12 +0530

    Final fix for utils types

--- campus-anon/lib/utils.ts
+++ campus-anon/lib/utils.ts
@@ -27,7 +27,7 @@ export interface Message {
   id: string;
   created_at: string;
   sender_id: string;
-  receiver_id: string;
+  recipient_id: string; 
   content: string;
   read_at?: string | null;
 }
